d80549faea77e97972c64613591ab124e57cd296
OSU <whysabi492@naver.com>
2024-03-10 19:39:31 +0900

    Add attachment, issue repository tests

---
+++ src/test/java/site/jobiljeong/scheduler/repository/AttachmentTest.java
@@ -0,0 +1,59 @@
+package site.jobiljeong.scheduler.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import site.jobiljeong.scheduler.entity.Attachment;
+import site.jobiljeong.scheduler.entity.Company;
+import site.jobiljeong.scheduler.entity.Schedule;
+import site.jobiljeong.scheduler.entity.custom.ScheduleType;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+public class AttachmentTest {
+
+    @Autowired
+    private AttachmentRepository attachmentRepository;
+
+    @Test
+    void insertAttachmentSuccess() {
+        Attachment attachment = createAttachment();
+
+        Attachment savedAttachment = attachmentRepository.save(attachment);
+
+        assertThat(savedAttachment.getName()).isEqualTo("AttachmentName");
+        assertThat(savedAttachment.getCategory()).isEqualTo("AttachmentCategory");
+        assertThat(savedAttachment.getAddress()).isEqualTo("AttachmentAddress");
+    }
+
+    private Attachment createAttachment() {
+        Schedule schedule = createSchedule();
+        return Attachment.builder()
+                .name("AttachmentName")
+                .category("AttachmentCategory")
+                .address("AttachmentAddress")
+                .schedule(schedule)
+                .build();
+    }
+
+    private Schedule createSchedule() {
+        Company company = createCompany();
+        return Schedule.builder()
+                .scheduleType(ScheduleType.MEETING)
+                .scheduleGroup("ScheduleGroup")
+                .websiteAddress("ScheduleWebsiteAddress")
+                .memo("ScheduleMemo")
+                .company(company)
+                .build();
+    }
+
+    private Company createCompany() {
+        return Company.builder()
+                .name("CompanyName")
+                .websiteAddress("CompanyWebsiteAddress")
+                .memo("CompanyMemo")
+                .build();
+    }
+}
---
+++ src/test/java/site/jobiljeong/scheduler/repository/IssueTest.java
@@ -0,0 +1,44 @@
+package site.jobiljeong.scheduler.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import site.jobiljeong.scheduler.entity.Company;
+import site.jobiljeong.scheduler.entity.Issue;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+public class IssueTest {
+
+    @Autowired
+    private IssueRepository issueRepository;
+
+    @Test
+    void insertIssueTest() {
+        Issue issue = createIssue();
+
+        Issue savedIssue = issueRepository.save(issue);
+
+        assertThat(savedIssue.getOriginLink()).isEqualTo("IssueOriginLink");
+        assertThat(savedIssue.getMemo()).isEqualTo("IssueMemo");
+    }
+
+    private Issue createIssue() {
+        Company company = createCompany();
+        return Issue.builder()
+                .originLink("IssueOriginLink")
+                .memo("IssueMemo")
+                .company(company)
+                .build();
+    }
+
+    private Company createCompany() {
+        return Company.builder()
+                .name("CompanyName")
+                .websiteAddress("CompanyWebsiteAddress")
+                .memo("CompanyMemo")
+                .build();
+    }
+}
