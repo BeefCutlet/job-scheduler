1c4ab9ab14d6ca60921f3ee295fc2472762a7d13
OSU <whysabi492@naver.com>
2024-03-14 00:39:06 +0900

    Add dto for saving user info

---
+++ src/main/java/site/jobiljeong/scheduler/dto/UserSaveRequest.java
@@ -0,0 +1,25 @@
+package site.jobiljeong.scheduler.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import site.jobiljeong.scheduler.entity.Users;
+import site.jobiljeong.scheduler.entity.custom.UserStatus;
+
+@Getter
+@AllArgsConstructor
+public class UserSaveRequest {
+
+    private String uid;
+    private String password;
+    private String nickname;
+
+    public Users convertToEntity() {
+        return Users.builder()
+                .uid(this.uid)
+                .password(this.password)
+                .nickname(this.nickname)
+                .authority("ROLE_USER")
+                .status(UserStatus.ACTIVE)
+                .build();
+    }
+}
