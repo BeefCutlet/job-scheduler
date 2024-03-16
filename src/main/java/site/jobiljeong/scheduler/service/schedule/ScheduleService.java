dcbccd656c1c29e415623e48e5482a3cad8ce1f6
OSU <whysabi492@naver.com>
2024-03-17 00:06:19 +0900

    Modify findScheduleList method

--- src/main/java/site/jobiljeong/scheduler/service/schedule/ScheduleService.java
+++ src/main/java/site/jobiljeong/scheduler/service/schedule/ScheduleService.java
@@ -3,18 +3,18 @@ package site.jobiljeong.scheduler.service.schedule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.jobiljeong.scheduler.dto.ScheduleInfo;
 import site.jobiljeong.scheduler.dto.ScheduleInfoRequest;
+import site.jobiljeong.scheduler.dto.ScheduleInfoResponse;
 import site.jobiljeong.scheduler.dto.ScheduleSaveRequest;
 import site.jobiljeong.scheduler.dto.ScheduleUpdateRequest;
 import site.jobiljeong.scheduler.entity.Company;
 import site.jobiljeong.scheduler.entity.Schedule;
 import site.jobiljeong.scheduler.entity.Users;
 import site.jobiljeong.scheduler.repository.company.CompanyRepository;
+import site.jobiljeong.scheduler.repository.schedule.ScheduleQueryRepository;
 import site.jobiljeong.scheduler.repository.schedule.ScheduleRepository;
 import site.jobiljeong.scheduler.repository.users.UsersRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,8 +23,9 @@ import java.util.List;
 public class ScheduleService {
 
     private final UsersRepository usersRepository;
-    private final ScheduleRepository scheduleRepository;
     private final CompanyRepository companyRepository;
+    private final ScheduleRepository scheduleRepository;
+    private final ScheduleQueryRepository scheduleQueryRepository;
 
     /**
      * 일정 등록 + 일정과 연관된 회사 등록 (이름만)
@@ -53,18 +54,17 @@ public class ScheduleService {
     /**
      * 일정 정보 단건 조회
      */
-    public ScheduleInfo findSchedule(Long scheduleNo) {
+    public ScheduleInfoResponse findSchedule(Long scheduleNo) {
         Schedule schedule = scheduleRepository.findById(scheduleNo).orElseThrow(
                 () -> new IllegalArgumentException("일정 정보를 찾을 수 없습니다."));
 
-        return new ScheduleInfo().createScheduleInfo(schedule);
+        return new ScheduleInfoResponse().createScheduleInfo(schedule);
     }
 
     /**
      * 일정 정보 목록 조회 (기간별 - 월별)
      */
-    public List<ScheduleInfo> findScheduleList(Long userId, ScheduleInfoRequest scheduleInfoRequest) {
-        List<ScheduleInfo> list = new ArrayList<>();
-        return list;
+    public List<ScheduleInfoResponse> findScheduleList(Long userId, ScheduleInfoRequest scheduleInfoRequest) {
+        return scheduleQueryRepository.findScheduleList(userId, scheduleInfoRequest.getRequestDate());
     }
 }
