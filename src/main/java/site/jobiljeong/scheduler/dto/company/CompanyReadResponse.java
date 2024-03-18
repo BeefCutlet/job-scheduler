c5d2ff60cd49087841845dfb8289cce84b396316
OSU <whysabi492@naver.com>
2024-03-18 12:20:45 +0900

    Add constructor in company dto

--- src/main/java/site/jobiljeong/scheduler/dto/company/CompanyReadResponse.java
+++ src/main/java/site/jobiljeong/scheduler/dto/company/CompanyReadResponse.java
@@ -1,17 +1,31 @@
 package site.jobiljeong.scheduler.dto.company;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import site.jobiljeong.scheduler.entity.Issue;
+import site.jobiljeong.scheduler.dto.issue.IssueReadResponse;
+import site.jobiljeong.scheduler.entity.Company;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class CompanyReadResponse {
 
     private String name;
     private String websiteAddress;
     private String memo;
-    private List<Issue> issues;
+    private List<IssueReadResponse> issues;
+
+    public CompanyReadResponse(String name, String websiteAddress, String memo) {
+        this.name = name;
+        this.websiteAddress = websiteAddress;
+        this.memo = memo;
+    }
+
+    public CompanyReadResponse(Company company) {
+        this.name = company.getName();
+        this.websiteAddress = company.getWebsiteAddress();
+        this.memo = company.getMemo();
+        this.issues = company.getIssues().stream().map(
+                issue -> new IssueReadResponse(issue.getOriginLink(), issue.getMemo())).collect(Collectors.toList());
+    }
 }
