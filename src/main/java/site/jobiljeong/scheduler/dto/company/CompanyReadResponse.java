ffd47ecb615906714d5c1fa06d8709dae9c837fa
OSU <whysabi492@naver.com>
2024-03-18 13:11:39 +0900

    Rename factory method in company dto

--- src/main/java/site/jobiljeong/scheduler/dto/company/CompanyReadResponse.java
+++ src/main/java/site/jobiljeong/scheduler/dto/company/CompanyReadResponse.java
@@ -1,5 +1,6 @@
 package site.jobiljeong.scheduler.dto.company;
 
+import lombok.Builder;
 import lombok.Getter;
 import site.jobiljeong.scheduler.dto.issue.IssueReadResponse;
 import site.jobiljeong.scheduler.entity.Company;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
+@Builder
 public class CompanyReadResponse {
 
     private String name;
@@ -15,17 +17,13 @@ public class CompanyReadResponse {
     private String memo;
     private List<IssueReadResponse> issues;
 
-    public CompanyReadResponse(String name, String websiteAddress, String memo) {
-        this.name = name;
-        this.websiteAddress = websiteAddress;
-        this.memo = memo;
-    }
-
-    public CompanyReadResponse(Company company) {
-        this.name = company.getName();
-        this.websiteAddress = company.getWebsiteAddress();
-        this.memo = company.getMemo();
-        this.issues = company.getIssues().stream().map(
-                issue -> new IssueReadResponse(issue.getOriginLink(), issue.getMemo())).collect(Collectors.toList());
+    public static CompanyReadResponse of(Company company) {
+        return CompanyReadResponse.builder()
+                .name(company.getName())
+                .websiteAddress(company.getWebsiteAddress())
+                .memo(company.getMemo())
+                .issues(company.getIssues().stream().map(
+                        issue -> new IssueReadResponse(issue.getOriginLink(), issue.getMemo())).collect(Collectors.toList()))
+                .build();
     }
 }
