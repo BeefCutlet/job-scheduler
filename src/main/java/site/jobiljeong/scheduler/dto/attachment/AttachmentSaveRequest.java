8724fe5b38adde85a3ce0d98e72c5c8113aab1f8
OSU <whysabi492@naver.com>
2024-03-20 22:39:47 +0900

    Add dto of attachment

---
+++ src/main/java/site/jobiljeong/scheduler/dto/attachment/AttachmentSaveRequest.java
@@ -0,0 +1,26 @@
+package site.jobiljeong.scheduler.dto.attachment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import site.jobiljeong.scheduler.entity.Attachment;
+import site.jobiljeong.scheduler.entity.Schedule;
+import site.jobiljeong.scheduler.entity.custom.AttachmentCategory;
+
+@Getter
+@AllArgsConstructor
+public class AttachmentSaveRequest {
+
+    private Long scheduleNo;
+    private String name;
+    private AttachmentCategory category;
+    private String url;
+
+    public Attachment createAttachment(Schedule schedule) {
+        return Attachment.builder()
+                .name(name)
+                .category(category)
+                .url(url)
+                .schedule(schedule)
+                .build();
+    }
+}
