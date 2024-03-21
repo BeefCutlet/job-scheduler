182b22b6a166200fcba2a19348a50ee74a371012
OSU <whysabi492@naver.com>
2024-03-22 01:09:36 +0900

    Modify return of FileService

--- src/main/java/site/jobiljeong/scheduler/util/FileService.java
+++ src/main/java/site/jobiljeong/scheduler/util/FileService.java
@@ -1,9 +1,10 @@
 package site.jobiljeong.scheduler.util;
 
 import org.springframework.web.multipart.MultipartFile;
+import site.jobiljeong.scheduler.dto.FileInfo;
 
 public interface FileService {
 
-    String storeFile(MultipartFile file);
+    FileInfo storeFile(MultipartFile file);
     void deleteFile(String filename);
 }
