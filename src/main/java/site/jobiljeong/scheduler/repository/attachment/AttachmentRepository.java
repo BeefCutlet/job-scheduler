aed6369e3aaddb91b90bd81c1857acb9c289366f
OSU <whysabi492@naver.com>
2024-03-21 21:14:59 +0900

    Add new method of attachment repository

--- src/main/java/site/jobiljeong/scheduler/repository/attachment/AttachmentRepository.java
+++ src/main/java/site/jobiljeong/scheduler/repository/attachment/AttachmentRepository.java
@@ -3,5 +3,8 @@ package site.jobiljeong.scheduler.repository.attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.jobiljeong.scheduler.entity.Attachment;
 
+import java.util.List;
+
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+    List<Attachment> findAllByScheduleId(Long scheduleId);
 }
